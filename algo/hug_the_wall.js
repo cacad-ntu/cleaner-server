bedb42f572154b0a116d43d88aa5c5e434346f14
dbakti7 <dbakti1605@gmail.com>
2017-07-12 00:16:35 +0800

    Added constants for actions and algo function declaration.

---
+++ algo/hug_the_wall.js
@@ -0,0 +1,8 @@
+var utils = require('../utils/utils.js')
+var Robot = require('./robot.js')
+var constants = require('../utils/constants.js')
+
+exports.hugTheWall = function(sensors, robot, currentMap, prevAction) {
+    // TODO: Implement this
+    return constants.FORWARD
+}
--- utils/constants.js
+++ utils/constants.js
@@ -9,3 +9,8 @@ define("DIR_UP", 0)
 define("DIR_RIGHT", 1)
 define("DIR_DOWN", 2)
 define("DIR_LEFT", 3)
+
+define("FORWARD", 0)
+define("TURN_RIGHT", 1)
+define("BACKWARD", 2)
+define("TURN_LEFT", 3)
